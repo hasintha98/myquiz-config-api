8d1bd794e70db3c0dfcdb75fc069aaa0e577b4fa
hasinthaSnc <139563894+hasinthaSnc@users.noreply.github.com>
2023-12-16 19:12:00 +0530

    save changes

--- src/integration/modules/integration.module.ts
+++ src/integration/modules/integration.module.ts
@@ -3,10 +3,11 @@ import { IntegrationController } from '../controllers/integration.controller';
 import { IntegrationService } from '../services/integration.service';
 import { MyLogger } from 'src/logger/logger.service';
 import { UserService } from 'src/user/services/user.service';
+import { QuestionsService } from 'src/questions/services/questions.service';
 
 @Module({
   imports: [],
   controllers: [IntegrationController],
-  providers: [IntegrationService, MyLogger, UserService],
+  providers: [IntegrationService, MyLogger, UserService, QuestionsService],
 })
 export class IntegrationModule {}
