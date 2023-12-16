bae13a4e44a1d49530edd26b1bff9f9e8a9125be
hasinthaSnc <139563894+hasinthaSnc@users.noreply.github.com>
2023-12-16 19:47:27 +0530

    save changes

--- src/integration/controllers/integration.controller.ts
+++ src/integration/controllers/integration.controller.ts
@@ -1,34 +1,35 @@
-import {
-  Body,
-    Controller,
-    Get,
-    Post
-  } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { IntegrationService } from '../services/integration.service';
 import { RegiterUserRequestDTO } from '../dto/RegisterUser.request.dto';
 import { VerifyAnswerRequestDTO } from '../dto/VerifyAnswers.request';
+import { AppService } from 'src/app.service';
+import { MyLogger } from 'src/logger/logger.service';
 
-  @Controller('integration')
-  export class IntegrationController {
-    constructor(private readonly integrationService: IntegrationService) {}
-  
-    @Post('/user/register')
-    registerUser(@Body() userDetails: RegiterUserRequestDTO) {
-      return this.integrationService.registerUser(userDetails);
-    }
+@Controller('integration')
+export class IntegrationController {
+  constructor(
+    private readonly integrationService: IntegrationService,
+    private readonly logger: MyLogger,
+  ) {}
 
-     
-    @Post('/user/unregister')
-    unregisterUser(@Body() userDetails: RegiterUserRequestDTO) {
-      return this.integrationService.unregisterUser(userDetails);
-    }
-
-
-    @Post('/user/answer/verify')
-    verifyAnswersByUser(@Body() answerRequestDTO: VerifyAnswerRequestDTO) {
-      return this.integrationService.answerVerification(answerRequestDTO);
-    }
-  
-  
+  @Post('/user/register')
+  registerUser(@Body() userDetails: RegiterUserRequestDTO) {
+    return this.integrationService.registerUser(userDetails);
   }
-  
+
+  @Post('/user/unregister')
+  unregisterUser(@Body() userDetails: RegiterUserRequestDTO) {
+    return this.integrationService.unregisterUser(userDetails);
+  }
+
+  @Post('/user/answer/verify')
+  verifyAnswersByUser(@Body() answerRequestDTO: VerifyAnswerRequestDTO) {
+    return this.integrationService.answerVerification(answerRequestDTO);
+  }
+
+  @Post('/user/external')
+  UserFromExternal(@Body() body: any) {
+    this.logger.log(`EXTERNAL USER| ${JSON.stringify(body)}`, AppService.name);
+    return body;
+  }
+}
